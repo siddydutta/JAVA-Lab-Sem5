d9c81d3e6333196df1905e65a90b2004d79a0960
Siddhartha Dutta <siddharthadutta@protonmail.com>
2019-12-05 14:01:55 +0530

    Update DatabaseConnectivity.java

--- DatabaseConnectivity.java
+++ DatabaseConnectivity.java
@@ -1,11 +1,11 @@
 import java.sql.*;
-public class DatabaseConn
+public class DatabaseConnectivity
 {
 	public static void main(String[] args)
 	{
 		try
 		{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver"); // Loads MySQL Driver
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sample","root","1234");
 			Statement s = con.createStatement();
 			ResultSet rs = s.executeQuery("select * from student");
